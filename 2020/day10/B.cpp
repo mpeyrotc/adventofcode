75c81bf40c66b382a59d41a86ccb5141a2e69679
Marco Peyrot Carrion <mapeyrot@microsoft.com>
2020-12-10 03:52:27 -0800

    cleanup part B

--- 2020/day10/B.cpp
+++ 2020/day10/B.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 typedef long long ll;
 
-int main2()
+int main()
 {
 	vector<int> jolts{ 0 };
 
@@ -22,29 +22,24 @@ int main2()
 	// Add device jolt
 	jolts.push_back(*(jolts.cend() - 1) + 3);
 
+	// Will used to keep track of previous combinations via DP.
 	vector<ll> combinations(jolts.size());
+
 	// There is at least one combination since we already past part 1.
 	combinations.at(0) = 1; 
 
-	for (int i{ 1 }; i < jolts.size(); ++i)
+	for (int i{ 0 }; i < jolts.size(); ++i)
 	{
-		ll currentCombinationCount{ 0 };
-		for (int j{ i }; j >= (i - 3) && j >= 0; --j)
+		for (int j{ i + 1 }; j <= (i + 3) && j < jolts.size(); ++j)
 		{
-			if (abs(jolts.at(i) - jolts.at(j)) <= 3)
+			if ((jolts.at(j) - jolts.at(i)) <= 3)
 			{
-				currentCombinationCount += combinations.at(j);
-			}
-			else
-			{
-				break;
+				combinations.at(j) += combinations.at(i);
 			}
 		}
-
-		combinations.at(i) = currentCombinationCount;
 	}
 
-	cout << combinations.at(combinations.size() - 1) << endl;
+	cout << *(combinations.cend() - 1) << endl;
 
 	return 0;
 }
